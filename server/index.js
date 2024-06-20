24ad1ef52a8857516d39cb6b38dc6ef7512674e4
Delving-Mage <147742069+Delving-Mage@users.noreply.github.com>
2024-06-20 14:35:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,15 +17,26 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-  socket.emit("network_interfaces", os.networkInterfaces());
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User with ID: ${socket.id} joined room: ${room}`);
   });
 
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("video_control", (data) => {
+    io.to(data.room).emit("control_video", data);
+  });
+
+  socket.on("iframe_url1", (data) => {
+    io.to(data.room).emit("update_iframe_url1", data.url);
+  });
+
+  socket.on("iframe_url2", (data) => {
+    io.to(data.room).emit("update_iframe_url2", data.url);
   });
 
   socket.on("disconnect", () => {
